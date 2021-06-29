efacb4d0848123295e4841f71dffe9b2f76e6b75
vapvarun <vapvarun@gmail.com>
2021-06-29 10:26:47 +0530

    Initial Commit

---
+++ includes/class- recaptcha-for-woocommerce-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://wbcomdesigns.com/
+ * @since      1.0.0
+ *
+ * @package    Recaptcha_For_Woocommerce
+ * @subpackage Recaptcha_For_Woocommerce/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Recaptcha_For_Woocommerce
+ * @subpackage Recaptcha_For_Woocommerce/includes
+ * @author     Wbcom Designs <admin@wbcomdesigns.com>
+ */
+class Recaptcha_For_Woocommerce_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class- recaptcha-for-woocommerce-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://wbcomdesigns.com/
+ * @since      1.0.0
+ *
+ * @package    Recaptcha_For_Woocommerce
+ * @subpackage Recaptcha_For_Woocommerce/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Recaptcha_For_Woocommerce
+ * @subpackage Recaptcha_For_Woocommerce/includes
+ * @author     Wbcom Designs <admin@wbcomdesigns.com>
+ */
+class Recaptcha_For_Woocommerce_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
