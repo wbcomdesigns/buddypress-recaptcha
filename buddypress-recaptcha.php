6e1bd66119edb736b749975299c61540e357ae90
Varun Kumar Dubey <vapvarun@gmail.com>
2021-08-16 16:04:35 +0530

    Update buddypress-recaptcha.php

--- buddypress-recaptcha.php
+++ buddypress-recaptcha.php
@@ -20,7 +20,7 @@
  * Author URI:        https://wbcomdesigns.com/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:        buddypress-recaptcha
+ * Text Domain:       buddypress-recaptcha
  * Domain Path:       /languages
  */
 
