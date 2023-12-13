5bce743f28e9ab926159dbdf6bc3d0d7a60eef11
Abhishek <pallavi@wbcomdesigns.com>
2023-12-13 16:44:57 +0530

    Updated top banner and links

--- admin/wbcom/templates/wbcom-plugins-page.php
+++ admin/wbcom/templates/wbcom-plugins-page.php
@@ -1,7 +1,7 @@
 <div class="wrap">
 	<div class="wbcom-bb-plugins-offer-wrapper">
 		<div id="wb_admin_logo">
-			<a href="https://wbcomdesigns.com/downloads/buddypress-community-bundle/" target="_blank">
+			<a href="https://wbcomdesigns.com/downloads/buddypress-community-bundle/?utm_source=pluginoffernotice&utm_medium=community_banner" target="_blank">
 				<img src="<?php echo esc_url( RFB_PLUGIN_URL ) . 'admin/wbcom/assets/imgs/wbcom-offer-notice.png'; ?>">
 			</a>
 		</div>
