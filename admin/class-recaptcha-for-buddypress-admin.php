9c8665d088cb89afd8018d923fc2568ea9758704
Nitin Patil <pallavi@wbcomdesigns.com>
2025-04-11 16:43:17 +0530

    Remove unwanted files and code of wbcom folder
    https://3.basecamp.com/5798509/buckets/37595344/card_tables/cards/8535395247

--- admin/class-recaptcha-for-buddypress-admin.php
+++ admin/class-recaptcha-for-buddypress-admin.php
@@ -165,9 +165,6 @@ class Recaptcha_For_BuddyPress_Admin {
 		<div class="wrap">
 			<div class="wbcom-bb-plugins-offer-wrapper">
 				<div id="wb_admin_logo">
-					<a href="https://wbcomdesigns.com/downloads/buddypress-community-bundle/?utm_source=pluginoffernotice&utm_medium=community_banner" target="_blank">
-						<img src="<?php echo esc_url( RFB_PLUGIN_URL ) . 'admin/wbcom/assets/imgs/wbcom-offer-notice.png'; ?>">
-					</a>
 				</div>
 			</div>
 			<div class="wbcom-wrap">
